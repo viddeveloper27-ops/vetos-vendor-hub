84ec23a5758004f339aab8d49be04d1f1d533f60
Akram Ali <viddeveloper27@gmail.com>
2026-04-09 12:56:21 +0530

    chore: revert API_URL to production staging endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-// export const API_URL = "https://api.vetos.stageprojects.xyz"
-export const API_URL = "https://vetos-api-saloni.coderly.in"
+export const API_URL = "https://api.vetos.stageprojects.xyz"
+// export const API_URL = "https://vetos-api-saloni.coderly.in"
 // export const API_URL = "http://api-vetos-akram.abhianand.in"
